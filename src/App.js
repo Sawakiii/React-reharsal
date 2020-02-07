cd95797bae4e631ff6b3c0ab2f4df2aba778f78b
Sawaki <you@example.com>
2020-02-07 11:54:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Reactのインポート。jsxが書けるようになる。
+import React from "react"
 
-function App() {
+// 他のコンポーネントをインポートする。
+import Form from "./Form"
+import List from "./List"
+
+// コンポーネントの定義
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Form></Form>
+    <List></List>
+    </>
+  )
 }
 
-export default App;
+
+
+// class App extends React.Component {
+//   render() {
+//     return(
+//       <div>aaaaaa</div>
+//     )
+//   }
+// }
+
+// コンポーネントのエクスポート
+export default App
+
+
