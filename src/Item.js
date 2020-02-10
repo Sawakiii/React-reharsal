a89e35c72c49a248b91ebd0dbd082f51bc2dccd2
Sawaki <you@example.com>
2020-02-10 10:21:38 +0900

    完了・戻す機能の実装

--- src/Item.js
+++ src/Item.js
@@ -1,8 +1,22 @@
 import React from "react"
 
 const Item = (props) => {
+
+    // 完了と戻すのためのステートを作成。
+    const [isDone, setIsDone] = React.useState(false)
+
+    // isDoneの値で完了と戻すが切替
+    const msg = isDone === false ? "完了" : "戻す"
+
+    const handleClick = () => {
+        // isDoneを反転させる。
+        setIsDone(!isDone)
+    }
     return (
-        <p>{props.todo}</p>
+        <>
+            <p>{props.todo}</p>
+            <button onClick={handleClick}>{msg}</button>
+        </>
     )
 }
 
