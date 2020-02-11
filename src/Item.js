b1ee1820a6dc45175425eadd199a8204d9e7011d
Sawaki <you@example.com>
2020-02-11 19:15:24 +0900

    取り消し線の機能を追加

--- src/Item.js
+++ src/Item.js
@@ -1,20 +1,31 @@
-import React from "react"
+import React, { useEffect } from "react"
+
+import styled from "styled-components"
+
+
+// スタイルの記述
+const P = styled.p`
+` 
+const Pdone = styled.p`
+    text-decoration: line-through;
+` 
 
 const Item = (props) => {
 
     // 完了と戻すのためのステートを作成。
     const [isDone, setIsDone] = React.useState(false)
-
-    // isDoneの値で完了と戻すが切替
     const msg = isDone === false ? "完了" : "戻す"
 
+    // ステートが変更されるとレンダリングが変更される。
+    const pTag = isDone ? <Pdone>{props.todo}</Pdone> : <P>{props.todo}</P>
+
     const handleClick = () => {
-        // isDoneを反転させる。
         setIsDone(!isDone)
     }
+
     return (
         <>
-            <p>{props.todo}</p>
+            {pTag}
             <button onClick={handleClick}>{msg}</button>
         </>
     )
