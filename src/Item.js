a71921146d2871e5b568020d08170fed8d83c300
Sawaki <you@example.com>
2020-02-12 11:20:10 +0900

    編集機能の実装

--- src/Item.js
+++ src/Item.js
@@ -14,10 +14,9 @@ const Item = (props) => {
 
     // 完了と戻すのためのステートを作成。
     const [isDone, setIsDone] = React.useState(false)
+    // 編集機能のためのステート
+    const [edit, setEdit] = React.useState(false)
     const msg = isDone === false ? "完了" : "戻す"
-
-    // ステートが変更されるとレンダリングが変更される。
-    const pTag = isDone ? <Pdone>{props.todo}</Pdone> : <P>{props.todo}</P>
 
     const handleClick = () => {
         setIsDone(!isDone)
@@ -32,11 +31,35 @@ const Item = (props) => {
         props.setTodos(newTodos)
     }
 
-    return (
-        <>
+    // ステートが変更されると関連する要素は再レンダリングされる。
+    // ステートが変更されるとレンダリングが変更される。
+    const pTag = isDone ? <Pdone>{props.todo}</Pdone> : <P>{props.todo}</P>
+
+    // 編集ステートが変更されるとレンダリングが変更される。
+    let element
+    if (edit) {
+        element = <form action="submit" onSubmit={(e)=>{
+            e.preventDefault()
+            const newTodos = props.todos.slice()
+            newTodos[props.id] = e.target.title.value
+            props.setTodos(newTodos)
+            setEdit(!edit)
+        }}>
+            <input type="text" placeholder={pTag} name="title"/>
+            <button type="submit">編集完了</button>
+        </form>
+    } else {
+        element = <div>
             {pTag}
             <button onClick={handleClick}>{msg}</button>
             <button onClick={handleDelete}>削除</button>
+            <button onClick={()=>{setEdit(!edit)}}>編集</button>
+        </div>
+    }
+
+    return (
+        <>
+            {element}
         </>
     )
 }
